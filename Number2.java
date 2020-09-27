98255f4ce6380962afa849921b0c9308a8df0d18
Santosh Bondre <SB065430@cerner.net>
2020-09-27 22:39:22 +0200

    java prog added

---
+++ Number2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Number2{
+  public static void main(String[]args) {
+    Scanner input = new Scanner(System.in);
+    int value = input.nextInt();
+
+      if (value < 0){
+        System.out.println("This is -ve number");
+      }
+       else if (value > 0){
+        System.out.println ("The number is positive");
+      }
+	else
+	{
+		System.out.println("The number is zero");
+	}
+	}
+}
